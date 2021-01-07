a98814eac464e04df0127029fa0561a8cad7112a
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-06 18:32:21 -0700

    Edit seed file residents.js

--- db/seeds/dev/residents.js
+++ db/seeds/dev/residents.js
@@ -7,7 +7,6 @@ exports.seed = function(knex) {
       // Inserts seed entries
     return knex('residents').insert([
     { 
-      "id": "1",
       "name": "Doris",
       "photo": "",
       "age": "87",
@@ -15,19 +14,17 @@ exports.seed = function(knex) {
       "weight": "145 lbs",
       "pcp": "Dr. Uppitomison",
       "ambulatory": "yes",
-      "help with medications": "yes",
-      "other impairments": "visual impairment",
-      "previous career": "Seamstress",
+      "help_with_medications": "yes",
+      "other_impairments": "visual impairment",
+      "previous_career": "Seamstress",
       "interests": "knitting, cats, card games, unsolved mysteries",
       "pets": "one cat",
-      "living preferences": "",
-      "appointments": [{"day": "Monday", "time": "1:00AM"}, {"day": "Thursday", "time": "9:00AM"}],
+      "living_preferences": "",
+      "appointments": JSON.stringify([{"day": "Monday", "time": "1:00AM"}, {"day": "Thursday", "time": "9:00AM"}]),
       "semester": "Spring 2021",
-      "additonal notes": "I need help feeding my cat, Mittens. Please don't eat peanuts around me, I am terribly allergic."
+      "additonal_notes": "I need help feeding my cat, Mittens. Please don't eat peanuts around me, I am terribly allergic."
     }
   ])
+  .catch(error => console.log(error))
 })
-.catch (error) {
-  console.log(`Error seeding data: ${error}`)
-}
 };
