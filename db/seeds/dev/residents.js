c9bda4ef604908c03095bf8cf8c2d06fdf31216a
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-06 18:14:49 -0700

    Move migration file into db; Create data structure

--- db/seeds/dev/residents.js
+++ db/seeds/dev/residents.js
@@ -1,16 +1,33 @@
 
-exports.seed = async function(knex) {
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  try {
-    await knex()
-  }
-  return knex('table_name').del()
+
+  return knex('residents').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+    return knex('residents').insert([
+    { 
+      "id": "1",
+      "name": "Doris",
+      "photo": "",
+      "age": "87",
+      "height": "5'9\"",
+      "weight": "145 lbs",
+      "pcp": "Dr. Uppitomison",
+      "ambulatory": "yes",
+      "help with medications": "yes",
+      "other impairments": "visual impairment",
+      "previous career": "Seamstress",
+      "interests": "knitting, cats, card games, unsolved mysteries",
+      "pets": "one cat",
+      "living preferences": "",
+      "appointments": [{"day": "Monday", "time": "1:00AM"}, {"day": "Thursday", "time": "9:00AM"}],
+      "semester": "Spring 2021",
+      "additonal notes": "I need help feeding my cat, Mittens. Please don't eat peanuts around me, I am terribly allergic."
+    }
+  ])
+})
+.catch (error) {
+  console.log(`Error seeding data: ${error}`)
+}
 };
