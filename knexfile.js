641abb463ec7ba3f409d3aafe5f5d5c09629b201
Cameron Romo <cromo1225@gmail.com>
2021-01-09 13:53:57 -0700

    Change configuration to client

--- knexfile.js
+++ knexfile.js
@@ -13,10 +13,13 @@ module.exports = {
     useNullAsDefault: true
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
