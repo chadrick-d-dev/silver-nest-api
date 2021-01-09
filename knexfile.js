303a0703ff4491a5022ea5995b5485cc723f4d9a
Cameron Romo <cromo1225@gmail.com>
2021-01-09 14:19:44 -0700

    Change ssl to not rejectUnauthorized

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL + `?ssl=true`,
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     migrations: {
       directory: './db/migrations'
     },
